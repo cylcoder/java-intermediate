a3342fe7eda637bead9cfd2e22d0dde1f1606131
cylcoder <cylcoder@gmail.com>
2024-09-09 11:05:54 +0900

    Section 5: Class 클래스

---
+++ src/main/java/lang/clazz/ClassCreateMain.java
@@ -0,0 +1,16 @@
+package lang.clazz;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class ClassCreateMain {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class helloClass = Hello.class;
+//        Class helloClass = Class.forName("lang.clazz.Hello");
+
+        Hello hello = (Hello) helloClass.getDeclaredConstructor().newInstance();
+        String result = hello.hello();
+        System.out.println("result = " + result);
+    }
+
+}
