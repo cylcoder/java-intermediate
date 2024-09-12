27319af8e3270d41bd63cfa3f6b11d108cbc2422
cylcoder <cylcoder@gmail.com>
2024-09-12 09:49:48 +0900

    Section 9: 지역 클래스 - 지역 변수 캡처3

--- src/main/java/nested/local/LocalOuterV4.java
+++ src/main/java/nested/local/LocalOuterV4.java
@@ -2,12 +2,12 @@ package nested.local;
 
 import java.lang.reflect.Field;
 
-public class LocalOuterV3 {
+public class LocalOuterV4 {
 
     private int outInstanceVar = 3;
 
     public Printer process(final int paramVar) {
-        final int localVar = 1; // 지역 변수는 스택 프레임이 종료되는 순간 함께 제거된다.
+        int localVar = 1;
 
         class LocalPrinter implements Printer {
             int value = 0;
@@ -19,17 +19,20 @@ public class LocalOuterV3 {
                 // 인스턴스는 지역 변수보다 더 오래 살아남는다.
                 System.out.println("localVar = " + localVar);
                 System.out.println("paramVar = " + paramVar);
+
                 System.out.println("outInstanceVar = " + outInstanceVar);
             }
         }
 
         LocalPrinter printer = new LocalPrinter();
-//        printer.print();를 여기서 실행하지 않고 Printer 인스턴스만 반환한다. 
+        // 만약 localVar의 값을 변경한다면? 다시 캡쳐해야 하나?
+//        localVar = 10; // 컴파일 오류
+//        paramVar = 20; // 컴파일 오류
         return printer;
     }
 
     public static void main(String[] args) {
-        LocalOuterV3 localOuter = new LocalOuterV3();
+        LocalOuterV4 localOuter = new LocalOuterV4();
         Printer printer = localOuter.process(2);
         // printer.print()를 나중에 실행한다. process()의 스택 프레임이 사라진 이후에 실행
         printer.print();
